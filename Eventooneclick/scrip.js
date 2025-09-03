28f32f99a1cf543f3846981b286cf658e8b32455
Ever Diaz <ever_2830@hotmail.com>
2025-09-03 10:10:52 -0300

    Add files via upload
    Evento onclick terminado 3/9/2025

---
+++ Eventooneclick/scrip.js
@@ -0,0 +1,12 @@
+function Inicio_de_Sesión(input){
+    input.innerText = "Cerrar Sesión"
+}
+
+function megusta(input){
+   alert("Gato Atigrado was liked")
+}
+
+function megusta2(input){
+   alert("Golden Retriever was liked")
+}
+
