2e0a0ca495bab30435359644c037f65002e778a4
Ever Diaz <ever_2830@hotmail.com>
2025-09-03 12:39:37 -0300

    Add files via upload

---
+++ Me gusta/script.js
@@ -0,0 +1,22 @@
+let lin_like = document.querySelector("#lin_like");
+let lina = document.querySelector("#lina");
+mateo_like  = document.querySelector("#mateo_like");
+
+let cont1 = 0;
+let cont2 = 0;
+let cont3 = 0;
+
+
+function contador1(){
+    cont1++;
+    Lin_like.innerText = cont1 + "Like(s)"
+
+}
+function contador2(){
+    cont2++;
+    Lina.innerText = cont2 + "Like(s)"
+}
+function contador3(){
+    cont3++;
+    Mateo_like.innerText = cont3 + "Like(s)"
+}
